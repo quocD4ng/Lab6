89dadb1e38a23c92c73c05fffe55f304cf8dd183
Lucastaa <76867086+Lucastaa@users.noreply.github.com>
2025-01-20 13:14:10 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Bài1
+{
+    //  Khởi tạo lớp Window
+    class Window
+    {
+        private int top;
+        private int left;
+        //  Attributes - thuộc tính trong lớp
+        public int Top
+        {
+            get => top;
+            set => top = value;
+        }
+        public int Left
+        {
+            get => left;
+            set => left = value;
+        }
+        //  Constructor mặc định không tham số
+        public Window()
+        {
+
+        }
+        public Window(int top, int left)
+        {
+            this.top = top;
+            this.left = left;
+        }
+        //  Phương thức đáp ứng yêu cầu đề bài
+        public virtual string DrawWindow()
+        {
+            return;
+        }
+    }
+    class Button:Window
+    {
+        public override string DrawWindow()
+        {
+            return;
+        }
+    }
+    class ListBox : Window
+    {
+        public string Content()
+        {
+            return;
+        }
+        public override string DrawWindow()
+        {
+            return;
+        }
+    }
+    class Program
+    {
+
+    }
+}
